6193e4f9fa904b174c8bfa2be76da34484b77151
icarpio <icarpiodeveloper@gmail.com>
2024-07-29 11:54:29 +0200

    Add data and icons

---
+++ HoroscopeIOS/Data/Horoscope.swift
@@ -0,0 +1,19 @@
+
+
+import Foundation
+import UIKit
+
+// Defining a structure
+struct Horoscope {
+   var id: String
+   var name: String
+   var logo: UIImage
+   var dates: String
+}
+
+
+
+
+
+
+
